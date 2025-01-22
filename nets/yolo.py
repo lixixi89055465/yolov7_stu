a400afb20afb54234d8bacfab02c1e5f1bd25f31
lixixi89055465 <nanji1850094299@outlook.com>
2025-01-22 23:06:32 +0800

    yolo

--- nets/yolo.py
+++ nets/yolo.py
@@ -8,6 +8,7 @@
 
 import torch.nn as nn
 from nets.backbone import SiLU, autopad, Conv, Backbone, Multi_Concat_Block, Transition_Block
+import numpy as np
 import torch
 
 
@@ -30,6 +31,180 @@ class RepConv(nn.Module):
         self.out_channels = c2
         assert k == 3
         assert autopad(k, p) == 1
+        padding_11 = autopad(k, p) - k // 2
+        self.act = nn.LeakyReLU(0.1, inplace=True) if act is True \
+            else (act if isinstance(act, nn.Module) else nn.Identity())
+        if deploy:
+            self.rbr_reparam = nn.Conv2d(c1, c2, k, s, autopad(k, p), groups=g, bias=True)
+        else:
+            self.rbr_identity = (nn.BatchNorm2d(num_features=c1, eps=0.001,
+                                                momentum=0.03) if c2 == c1 else None)
+            self.rbr_dense = nn.Sequential(
+                nn.Conv2d(c1, c2, k, s, autopad(k, p), groups=g, bias=False),
+                nn.BatchNorm2d(num_features=c2, eps=0.001, momentum=0.03)
+            )
+            self.rbr_1x1 = nn.Sequential(
+                nn.Conv2d(c1, c2, 1, s, padding_11, groups=g, bias=False),
+                nn.BatchNorm2d(num_features=c2, eps=0.001, momentum=0.03)
+            )
+
+    def forward(self, inputs):
+        if hasattr(self, 'rbr_reparam'):
+            return self.act(self.rbr_reparam(inputs))
+        if self.rbr_identity is None:
+            id_out = 0
+        else:
+            id_out = self.rbr_identity(inputs)
+        return self.act(self.rbr_dense(inputs) + self.rbr_1x1(inputs) + id_out)
+
+    def get_equivalent_kernel_bias(self):
+        kernel3x3, bias3x3 = self._fuse_bn_tensor(self.rbr_dense)
+        kernel1x1, bias1x1 = self._fuse_bn_tensor(self.rbr_1x1)
+        kernelid, biasid = self._fuse_bn_tensor(self.rbr_identity)
+        return (
+            kernel3x3 + self._pad_1x1_to_3x3_tensor(kernel1x1) + kernelid,
+            bias3x3 + bias1x1 + biasid,
+        )
+
+    def _pad_1x1_to_3x3_tensor(self, kernel1x1):
+        if kernel1x1 is None:
+            return 0
+        else:
+            return nn.functional.pad(kernel1x1, [1, 1, 1, 1])
+
+    def _fuse_bn_tensor(self, branch):
+        if branch is None:
+            return 0, 0
+        if isinstance(branch, nn.Sequential):
+            kernel = branch[0].weight
+            running_mean = branch[1].running_mean
+            running_var = branch[1].running_var
+            gamma = branch[1].weight
+            beta = branch[1].bias
+            eps = branch[1].eps
+        else:
+            assert isinstance(branch, nn.BatchNorm2d)
+            if not hasattr(self, 'id_tensor'):
+                input_dim = self.in_channels // self.groups
+                kernel_value = np.zeros(
+                    (self.in_channels, input_dim, 3, 3), dtype=np.float32
+                )
+                for i in range(self.in_channels):
+                    kernel_value[i, i % input_dim, 1, 1] = 1
+                self.id_tensor = torch.from_numpy(
+                    kernel_value
+                ).to(branch.weight.device)
+            kernel = self.id_tensor
+            running_mean = branch.running_mean
+            running_var = branch.running_var
+            gamma = branch.weight
+            beta = branch.bias
+            eps = branch.eps
+        std = (running_var + eps).sqrt()
+        t = (gamma / std).reshape(-1, 1, 1, 1)
+        return kernel * t, beta - running_mean * gamma / std
+
+    def repvgg_convert(self):
+        kernel, bias = self.get_equivalent_kernel_bias()
+        return (
+            kernel.detach().cpu().numpy(),
+            bias.detach().cpu().numpy()
+        )
+
+    def fuse_conv_bn(self, conv, bn):
+        std = (bn.running_var + bn.eps).sqrt()
+        bias = bn.bias - bn.running_mean * bn.weight / std
+        t = (bn.weight / std).reshape(-1, 1, 1, 1)
+        weights = conv.weight * t
+        bn = nn.Identity()
+        conv = nn.Conv2d(
+            in_channels=conv.in_channels,
+            out_channels=conv.out_channels,
+            kernel_size=conv.kernel_size,
+            stride=conv.stride,
+            padding=conv.padding,
+            dilation=conv.dilation,
+            groups=conv.groups,
+            bias=True,
+            padding_mode=conv.padding_mode
+        )
+        conv.weight = torch.nn.Parameter(weights)
+        conv.bias = torch.nn.Parameter(bias)
+        return conv
+
+    def fuse_repvgg_block(self):
+        if self.deploy:
+            return
+        print(f'RepConv.fuse_repvgg_block')
+        self.rbr_dense = self.fuse_conv_bn(
+            self.rbr_dense[0], self.rbr_dense[1]
+        )
+        self.rbr_1x1 = self.fuse_conv_bn(
+            self.rbr_1x1[0],
+            self.rbr_1x1[1]
+        )
+        rbr_1x1_bias = self.rbr_1x1.bias
+        weight_1x1_expanded = torch.nn.functional.pad(
+            self.rbr_1x1.weight,
+            [1, 1, 1, 1]
+        )
+        # Fuse self.rbr_identity
+        if (
+                isinstance(self.rbr_identity, nn.BatchNorm2d) \
+                or \
+                isinstance(self.rbr_identity, nn.modules.batchnorm.SyncBatchNorm)
+        ):
+            identity_conv_1x1 = nn.Conv2d(
+                in_channels=self.in_channels,
+                out_channels=self.out_channels,
+                kernel_size=1,
+                stride=1,
+                padding=0,
+                groups=self.groups,
+                bias=False
+            )
+            identity_conv_1x1.weight.data = (
+                identity_conv_1x1.weight.data.to(
+                    self.rbr_1x1.weight.data.device
+                ))
+            identity_conv_1x1.weight.data = (
+                identity_conv_1x1.weight.data.squeeze().squeeze()
+            )
+            identity_conv_1x1.weight.data.fill_(0.0)
+            identity_conv_1x1.weight.data.fill_diagonal_(0.0)
+            identity_conv_1x1.weight.data = (
+                identity_conv_1x1.weight.data.unsqueeze(2).unsqueeze(3))
+
+            identity_conv_1x1 = self.fuse_conv_bn(identity_conv_1x1, self.rbr_identity)
+            bias_identity_expanded = identity_conv_1x1.bias
+            weight_1x1_expanded = torch.nn.functional.pad(
+                identity_conv_1x1.weight,
+                [1, 1, 1, 1]
+            )
+        else:
+            bias_identity_expanded = torch.nn.Parameter(
+                torch.zeros_like(torch.zeros_like(rbr_1x1_bias))
+            )
+            weight_identity_expanded = torch.nn.Parameter(
+                torch.zeros_like(weight_1x1_expanded)
+            )
+        self.rbr_dense.weight = torch.nn.Parameter(
+            self.rbr_dense.weight + weight_1x1_expanded + weight_identity_expanded
+        )
+        self.rbr_dense.bias = torch.nn.Parameter(
+            self.rbr_dense.bias + rbr_1x1_bias + bias_identity_expanded
+        )
+        self.rbr_reparam = self.rbr_dense
+        self.deploy = True
+        if self.rbr_identity is not None:
+            del self.rbr_identity
+            self.rbr_identity = None
+        if self.rbr_1x1 is not None:
+            del self.rbr_1x1
+            self.rbr_1x1 = None
+        if self.rbr_dense is not None:
+            del self.rbr_dense
+            self.rbr_dense = None
 
 
 class SPPCSPC(nn.Module):
@@ -142,6 +317,31 @@ class YoloBody(nn.Module):
         )
         # ------------------------加强特征提取网络------------------------#
         # 80, 80, 128 => 80, 80, 256
+        self.rep_conv_1 = conv(transition_channels * 4, transition_channels * 8, 3, 1)
+        # 40, 40, 256 => 40, 40, 512
+        self.rep_conv_2 = conv(transition_channels * 8, transition_channels * 16, 3, 1)
+        # 20, 20, 512 => 20, 20, 1024
+        self.rep_conv_3 = conv(transition_channels * 16, transition_channels * 32, 3, 1)
+        # 4 + 1 + num_classes
+        # 80, 80, 256 => 80, 80, 3 * 25 (4 + 1 + 20) & 85 (4 + 1 + 80)
+        self.yolo_head_P3 = nn.Conv2d(transition_channels * 8,
+                                      len(anchors_mask[2]) * (5 + num_classes),
+                                      kernel_size=1)
+        # 40, 40, 512 => 40, 40, 3 * 25 & 85
+        self.yolo_head_P4 = \
+            nn.Conv2d(
+                transition_channels * 16,
+                len(anchors_mask[1]) * (5 + num_classes), 1)
+        # 20, 20, 512 => 20, 20, 3 * 25 & 85
+        self.yolo_head_P5 = \
+            nn.Conv2d(transition_channels * 32,
+                      len(anchors_mask[0]) * (5 + num_classes),
+                      1)
+
+    def fuse(self):
+        print("Fussing layers...")
+        for m in self.modules():
+            pass
 
     def forward(self, x):
         # backbone
@@ -190,3 +390,14 @@ class YoloBody(nn.Module):
         #   y3=(batch_size, 75, 80, 80)
         # ---------------------------------------------------#
         out2 = self.yolo_head_P3(P3)
+        # ---------------------------------------------------#
+        #   第二个特征层
+        #   y2=(batch_size, 75, 40, 40)
+        # ---------------------------------------------------#
+        out1 = self.yolo_head_P4(P4)
+        # ---------------------------------------------------#
+        #   第一个特征层
+        #   y1=(batch_size, 75, 20, 20)
+        # ---------------------------------------------------#
+        out0 = self.yolo_head_P5(P5)
+        return [out0, out1, out2]
