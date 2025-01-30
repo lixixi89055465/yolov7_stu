1ef606c48a1d8d5134057b27d25b5b934873952b
lixixi89055465 <nanji1850094299@outlook.com>
2025-01-30 22:34:54 +0800

    init

--- utils/callbacks.py
+++ utils/callbacks.py
@@ -69,5 +69,29 @@ class EvalCallback():
     def __init__(self,
                  net,
                  input_shape,
-                 anchor):
-        pass
+                 anchor,
+                 anchors_mask, class_names, num_classes,
+                 val_lines, log_dir, cuda, \
+                 map_out_path='.temp_map_out',
+                 max_boxes=100, confidence=0.05,
+                 nms_iou=0.5, letterbox_image=True,
+                 MINOVERLAP=0.5, eval_flag=True, period=1):
+        super(EvalCallback).__init__()
+        self.net = net
+        self.input_shape = input_shape
+        self.anchor = anchor
+        self.anchors_mask = anchors_mask
+        self.class_names = class_names
+        self.num_classes = num_classes
+        self.val_lines = val_lines
+        self.log_dir = log_dir
+        self.cuda = cuda
+        self.map_out_path = map_out_path
+        self.max_boxes = max_boxes
+        self.confidence = confidence
+        self.nms_iou = nms_iou
+        self.letterbox_image = letterbox_image
+        self.MINOVERLAP = MINOVERLAP
+        self.eval_flag = eval_flag
+        self.period = period
+        self.bbox_util = None
